779fd711cdf71e5de75f63ae66a86d77bf24f048
Joel <Joel@JOEL>
2019-03-12 10:43:52 +0100

    Fixed a bug when setting custom roles.

--- commands/role.command.ts
+++ commands/role.command.ts
@@ -10,31 +10,29 @@ export default class RoleCommand {
         return guild.adminRole;
     }
     setBotRole(guild: any, msg: discord.Message, name: string) {
-        if(this.validRole(guild, msg, name)){
-            if (guild.adminRole === null) { // setting adminRole = botRole until its manually set.
-                guild.adminRole = name;
-            }
-            new Bot().replyWith(msg, `\`WARNING\`: You have changed the admin role to: \`${name}\``);
+        if(this.validRole(msg, name)){
+            new Bot().replyWith(msg, `\`WARNING\`: You have changed the bot role to: \`${name}\``);
             return guild.botRole = name;
         } else {
             new Bot().replyWith(msg, `\`FAILED\`: This is not an exisiting role. You must create the \`${name}\` role first.`, 5000);
         }
     }
     setAdminRole(guild: any, msg: any, name: any) {
-        if(this.validRole(guild, msg, name)){
-            new Bot().replyWith(msg, `\`WARNING\`: You have changed the bot role to: \`${name}\``);
+        if(this.validRole(msg, name)){
+            new Bot().replyWith(msg, `\`WARNING\`: You have changed the admin role to: \`${name}\``);
             return guild.adminRole = name;
         } else {
             new Bot().replyWith(msg, `\`FAILED\`: This is not an exisiting role. You must create the \`${name}\` role first.`, 5000);
         }
     }
 
-    validRole(guild:discord.Guild, msg: discord.Message, roleName: string){
-        guild.roles.forEach(role => {
+    validRole(msg: discord.Message, roleName: string){
+        let botRole = false;
+        msg.guild.roles.forEach(role => {
             if(role.name.toLowerCase() == roleName.toLowerCase()){
-                return true;
+                botRole = true;
             }
         });
-        return false;
+        return botRole;
     }
 }
